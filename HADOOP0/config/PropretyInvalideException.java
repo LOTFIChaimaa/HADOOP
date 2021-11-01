43051033fd01b693e6d70397db999e23ebd921ec
LOTFIChaimaa <62658117+LOTFIChaimaa@users.noreply.github.com>
2021-11-01 21:30:35 +0000

    Add files via upload

---
+++ HADOOP0/config/PropretyInvalideException.java
@@ -0,0 +1,12 @@
+
+package config;
+
+public class PropretyInvalideException extends Exception {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public PropretyInvalideException(String msg) {
+		super(msg);
+	}
+}
